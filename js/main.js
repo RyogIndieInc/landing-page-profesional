9998d6f5e3380fd47bb636045f4798a4c58bcb8f
Ronny Olivares <ryogindieinc@gmail.com>
2025-08-23 15:31:36 -0500

    feat: Añadiendo el primer event listener para cambiar el color del título y eliminando el :hover del css, ya que desde ahora se manejará con js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 console.log("¡Hola, mundo! JavaScript funcionando.");
 
 const titulo = document.querySelector('.titulo-principal');
+titulo.addEventListener('click', () => {
+    titulo.style.color = '#d11241'; // Cambia el color a rojo
+});
